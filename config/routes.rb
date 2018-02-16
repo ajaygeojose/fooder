2dfe523a00cb6a2a32c182dcb36ebe064e4bbf6b
ajaygeojose <ajaygeojose@gmail.com>
2018-02-16 12:30:46 +0530

    added user model and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Fooder::Application.routes.draw do
+  get "users/login"
+  get "users/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
